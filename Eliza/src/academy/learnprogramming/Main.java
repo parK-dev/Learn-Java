654d52c8cdad4a826c83d6a733dda87fcf7612f7
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-15 22:44:51 +0900

    Working basic Eliza

--- Eliza/src/academy/learnprogramming/Main.java
+++ Eliza/src/academy/learnprogramming/Main.java
@@ -1,8 +1,20 @@
 package academy.learnprogramming;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var scanner = new Scanner(System.in);
+
+	    System.out.println(Doctor.intro());
+	    var userInput = "";
+
+	    while(!userInput.equalsIgnoreCase("quit")) {
+	    	userInput = scanner.nextLine();
+	    	String response = Doctor.response(userInput);
+	    	System.out.println(response);
+		}
+	    scanner.close();
     }
 }
